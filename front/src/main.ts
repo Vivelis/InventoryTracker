cb2b53c4cedaff24305319b65492a4bd058e129f
macgameur <maceo.jalbert@gmail.com>
2024-03-13 20:27:51 +0100

    Change karma tests to jest

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,32 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AppComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create the app', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it(`should have as title 'demo-angular-jest'`, () => {
+        expect(component.title).toEqual('demo-angular-jest');
+    });
+
+    it('should render the title', () => {
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.content span')?.textContent).toContain('demo-angular-jest app is running!');
+    });
+});
