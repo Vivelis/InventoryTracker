c549736c8c9094e7b2ac87da01adf38bea74fa1b
macgameur <maceo.jalbert@gmail.com>
2024-03-13 20:38:17 +0100

    Fix bad main conversion to test

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,32 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-describe('AppComponent', () => {
-    let component: AppComponent;
-    let fixture: ComponentFixture<AppComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [AppComponent],
-        }).compileComponents();
-    });
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(AppComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
-    it('should create the app', () => {
-        expect(component).toBeTruthy();
-    });
-
-    it(`should have as title 'demo-angular-jest'`, () => {
-        expect(component.title).toEqual('demo-angular-jest');
-    });
-
-    it('should render the title', () => {
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('demo-angular-jest app is running!');
-    });
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
